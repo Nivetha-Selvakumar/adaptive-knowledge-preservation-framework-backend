2a603b9f99e1c34f397b9b2f87802f7090f4d430
Nivetha-Selvakumar <nivetha.s@aaludra.com>
2026-07-19 23:43:28 +0530

    Initial backend project setup

---
+++ src/main/java/com/psg/adaptive/knowledge_preservation_backend/KnowledgePreservationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.psg.adaptive.knowledge_preservation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgePreservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgePreservationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psg/adaptive/knowledge_preservation_backend/KnowledgePreservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.psg.adaptive.knowledge_preservation_backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KnowledgePreservationBackendApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
